974dd1b8f9ce9868359b54bb30039433d934a07e
Marcus Ottosson <konstruktion@gmail.com>
2015-07-13 14:41:40 +0100

    Fixing compatibility on Linux

--- pythonpath/pyblish/__init__.py
+++ pythonpath/pyblish/__init__.py
@@ -1,5 +1,5 @@
 import os
 import imp
 
-util = imp.load_source("_util", os.path.join(__file__, "..", "..", "_util.py"))
+util = imp.load_source("_util", os.path.realpath(os.path.join(__file__, "..", "..", "_util.py")))
 util.wrap_module(__name__)
