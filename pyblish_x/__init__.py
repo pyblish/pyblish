e24b1e77e8197965d93a0fdcdcf70d2b226efeb4
Marcus Ottosson <konstruktion@gmail.com>
2015-07-18 09:43:52 +0200

    Repairing `install()`

--- pyblish_x/__init__.py
+++ pyblish_x/__init__.py
@@ -3,5 +3,5 @@ import sys
 
 
 def install():
-    pythonpath = os.path.join(__file__, "..", "..", "python")
+    pythonpath = os.path.join(__file__, "..", "..", "pythonpath")
     sys.path.insert(0, os.path.realpath(pythonpath))
