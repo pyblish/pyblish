48893d5f0a49df7127495aa3aeac714f30a0341a
Marcus Ottosson <konstruktion@gmail.com>
2016-01-31 16:55:21 +0000

    Avoid potential name clash

--- pyblish_x/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-
-def install():
-    pythonpath = os.path.join(__file__, "..", "..", "pythonpath")
-    sys.path.insert(0, os.path.realpath(pythonpath))
--- pythonpath/pyblish/__init__.py
+++ pythonpath/pyblish/__init__.py
@@ -1,5 +1,5 @@
 import os
 import imp
 
-util = imp.load_source("_util", os.path.realpath(os.path.join(__file__, "..", "..", "_util.py")))
+util = imp.load_source("__pyblish_util", os.path.realpath(os.path.join(__file__, "..", "..", "__pyblish_util.py")))
 util.wrap_module(__name__)
