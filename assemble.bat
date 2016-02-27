f1c69503f5420006466d511b474f8ce0f8da151a
Marcus Ottosson <konstruktion@gmail.com>
2016-02-27 14:47:46 +0000

    Update build script

--- assemble.bat
+++ assemble.bat
@@ -1,11 +1,12 @@
-set packages=pyblish-base ^
-             pyblish-hiero ^
-             pyblish-houdini ^
-             pyblish-integration ^
-             pyblish-maya ^
-             pyblish-nuke ^
-             pyblish-qml ^
-             pyblish-rpc ^
-             pyblish-standalone ^
-             pyblish-tray
-c:\python27\python.exe dist.py %CD%\pythonpath "%packages%" --clean
+@echo off
+set repos=pyblish-base ^
+          pyblish-hiero ^
+          pyblish-houdini ^
+          pyblish-integration ^
+          pyblish-maya ^
+          pyblish-nuke ^
+          pyblish-qml ^
+          pyblish-rpc ^
+          pyblish-standalone ^
+          pyblish-tray
+c:\python27\python.exe dist.py %CD%\pythonpath "%repos%" --clean
--- dist.py
+++ dist.py
@@ -14,24 +14,24 @@ import tempfile
 import zipfile
 
 
-def download_packages(root, packages):
-    """Download provided packages from GitHub
+def download_repos(root, repos):
+    """Download provided repos from GitHub
 
     Arguments:
         root (str): Destination directory for downloaded files
-        packages (iter): Names of packages to download
+        repos (iter): Names of repos to download
 
     Returns:
-        list: Absolute paths to downloaded packages
+        list: Absolute paths to downloaded repos
 
     Example:
-        >>> dirs = download_packages(
+        >>> dirs = download_repos(
         ...   "c:\tempdir", ["pyblish-base", "pyblish-qml"])
 
     """
 
     dirnames = list()
-    for package in packages:
+    for package in repos:
         url = "https://github.com/pyblish/%s/archive/master.zip" % package
         dst = os.path.join(root, package)
 
@@ -74,35 +74,48 @@ def generate_filelist(*packages):
                 yield src, dst
 
 
-def build(root, packages, clean=False):
-    """Build distribution at `root` of `packages`
+def build(root, repos, clean=False):
+    """Build distribution at `root` of `repos`
 
     Arguments:
         root (str): Directory into which to build distribution.
-        packages (str): Packages to include
+        repos (list): Packages to include
         clean (bool): Should `root` be erased before continuing?
 
     """
 
-    if os.path.exists(root):
+    tries = 3
+    while os.path.exists(root):
+        tries -= 1
+        if tries == 0:
+            return sys.stderr.write(
+                    "ERROR: Could not clean \"%s\", retrying..\n" % root)
+
         if not clean:
             return sys.stderr.write(
                 "ERROR: root directory already exists: %s\n" % root)
         else:
             try:
+                print("Cleaning up..")
                 shutil.rmtree(root)
             except OSError:
-                return sys.stderr.write(
-                    "ERROR: Could not clean \"%s\"\n" % root)
+                sys.stderr.write(
+                    "ERROR: Could not clean \"%s\", retrying..\n" % root)
 
-    modules = [package.replace("-", "_") for package in packages]
+    for dirname in download_repos(root=tempdir, repos=repos):
+        sys.path.insert(0, dirname)
 
-    for module in list(modules):
-        if module == "pyblish_base":
-            modules.remove(module)
-            modules.append("pyblish")
+    modules = [package.replace("-", "_") for package in repos]
 
-    for src, dst in generate_filelist(*packages):
+    try:
+        # Special case, Python package for
+        # "pyblish-base" is just "pyblish".
+        modules.remove("pyblish_base")
+        modules.append("pyblish")
+    except ValueError:
+        pass
+
+    for src, dst in generate_filelist(*modules):
         abspath = os.path.join(root, dst)
         dirname = os.path.dirname(abspath)
 
@@ -111,27 +124,27 @@ def build(root, packages, clean=False):
         except OSError:
             pass
 
-        print("Writing %s" % abspath)
+        print("Writing \"%s\"" % abspath)
         shutil.copyfile(src, abspath)
 
-    print("Done.")
+    print("Done")
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("root")
-    parser.add_argument("packages")
+    parser.add_argument("repos")
     parser.add_argument("--clean", action="store_true")
 
     args = parser.parse_args()
 
     tempdir = tempfile.mkdtemp()
-    packages = args.packages.split()
+    repos = args.repos.split()
 
-    for dirname in download_packages(root=tempdir, packages=packages):
-        sys.path.insert(0, dirname)
+    print("Building %s @ \"%s\", clean=%s" % (
+        repos, args.root, args.clean))
 
     build(root=args.root,
-          packages=args.packages.split(),
+          repos=repos,
           clean=args.clean)
