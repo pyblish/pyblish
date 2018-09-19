5d10a3ec5b68be7eb329cf02625af0798b9f3d00
Alan Fregtman <941331+darkvertex@users.noreply.github.com>
2018-09-19 13:20:58 -0400

    bump version.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers = [
 
 setup(
     name="pyblish",
-    version="1.5.4",
+    version="1.6.0",
     description="Plug-in driven automation framework for content",
     long_description="Collection of latest supported "
                      "combinations of Pyblish projects",
