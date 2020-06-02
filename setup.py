ef8d13ff90f26c4d4f8c189428d5946636798933
Marcus Ottosson <konstruktion@gmail.com>
2020-06-02 07:42:55 +0100

    Version up

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers = [
 
 setup(
     name="pyblish",
-    version="1.6.0",
+    version="1.6.1",
     description="Plug-in driven automation framework for content",
     long_description="Collection of latest supported "
                      "combinations of Pyblish projects",
